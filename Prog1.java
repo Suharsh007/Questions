71bdf9b1144d8a51379e3b5e3fd85d1bbaeafa96
Suharsh007 <64641725+Suharsh007@users.noreply.github.com>
2020-08-31 08:54:24 +0530

    2 Questions Done

---
+++ Prog1.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import java.util.*;
+
+public class  Prog1 {
+    public static void main(String args[])
+
+{
+    Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int[][] a = new int[n][n];
+        for (int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                a[i][j]= sc.nextInt();
+            }
+        }
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int j=0;j<n;j++)
+        {
+            map.put(a[0][j],1);
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(map.get(a[i][j])!=null && map.get(a[i][j])==i)
+                {
+                    map.put(a[i][j],i+1);
+                    if(i==n-1)
+                    System.out.print(a[i][j]+" ");
+                }
+            }
+
+        }
+
+}
+
+}
---
+++ Prog4.java
@@ -0,0 +1,120 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+class LinkedList
+{
+    static Node head;
+ 
+    static class Node{
+        int data;
+        Node next;
+ 
+        public Node(int d){
+            data = d;
+        }
+    }
+ 
+    static LinkedList insert(LinkedList list, int data){
+        Node new_node = new Node(data);
+        new_node.next = null;
+ 
+        if(list.head == null)
+            list.head = new_node;
+ 
+        else{
+            Node last = list.head;
+            while(last.next != null)
+                last = last.next;
+ 
+            last.next = new_node;
+        }
+        return list;
+    }
+ 
+    static int length(Node top){
+        Node curr = top;
+        if(curr.next == null)
+            return 0;
+ 
+        return 1 + length(curr.next);
+    }
+ 
+    static Node middle(Node top){
+        Node curr = top;
+        int len = length(top);
+        int count = 0;
+ 
+        while(curr != null){
+            if(count == len/2)
+                return curr;
+            count++;
+            curr = curr.next;
+        }
+        return null;
+    }
+ 
+    static void printList(Node top){
+        Node temp = top;
+ 
+        while(temp != null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+ 
+    static Node mergeList(Node first, Node second){
+        Node sorted = null;
+ 
+        if(first == null)
+            return second;
+        if(second == null)
+            return first;
+ 
+        if(first.data <= second.data){
+            sorted = first;
+            sorted.next = mergeList(first.next, second);
+        }
+        else{
+            sorted = second;
+            sorted.next = mergeList(first, second.next);
+        }
+ 
+        return sorted;
+    }
+ 
+    static Node sortList(Node top){
+        if(top == null || top.next == null)
+            return top;
+ 
+        Node mid = middle(top);
+        Node midNext = mid.next;
+ 
+        mid.next = null;
+ 
+        Node firstHalf = sortList(top);
+        Node secondHalf = sortList(midNext);
+ 
+        Node sorted = mergeList(firstHalf, secondHalf);
+ 
+        return sorted;
+    }
+ 
+	public static void main (String[] args) throws IOException
+	{
+		Scanner sc = new Scanner(System.in);
+ 
+		LinkedList list = new LinkedList();
+ 
+		int n =sc.nextInt();
+ 
+		
+        for(int i=0;i<n;i++){
+            int num = sc.nextInt();
+            list = insert(list, num);
+        }
+        head = sortList(head);
+        printList(head);
+ 
+	}
+}
